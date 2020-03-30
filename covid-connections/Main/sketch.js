4bdfd68994b3abd7cd4a79ccbeceeaf4756f1180
areskapoor <56699380+areskapoor@users.noreply.github.com>
2020-03-30 15:36:13 -0700

    fdsfads

---
+++ covid-connections/Main/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+
+}
+function draw() {
+  textAlign(CENTER)
+  rectMode(CENTER)
+  background(89, 85, 86)
+  noStroke();
+  fill(255)
+  textSize(48)
+  text("I Want To Help Others",width/2,height/15)
+  fill(203, 55, 58)
+  rect(width/2,height/2, width/1.1, height/1.25, 10)
+
+
+}
