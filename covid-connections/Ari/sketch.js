f7b98bc71ef16a6c411be2fe25f6ca8a28f0425d
areskapoor <56699380+areskapoor@users.noreply.github.com>
2020-04-02 15:41:52 -0700

    update

--- covid-connections/Ari/sketch.js
+++ covid-connections/Ari/sketch.js
@@ -1,3 +1,4 @@
+let button
 function setup() {
   createCanvas(windowWidth, windowHeight)
 
@@ -7,11 +8,38 @@ function draw() {
   rectMode(CENTER)
   background(89, 85, 86)
   noStroke();
+  //title text
   fill(255)
   textSize(48)
   text("I Want To Help Others",width/2,height/15)
+  //big box
   fill(203, 55, 58)
   rect(width/2,height/2, width/1.1, height/1.25, 10)
-
-
+  //subtitle text
+  fill(255)
+  textSize(36)
+  text("Tasks You Can Help With",width/2,height/6)
+  //tasks
+  function tasks(posX,posY,fullName,email,phoneNum,city,task) {
+    //main box text
+    rect(posX,posY,width/1.2,height/7,10)
+    //name text
+    fill(203, 55, 58)
+    textSize(20)
+    text(fullName,posX-width/3.25,posY-height/40)
+    //help rectangle & button
+    button = createButton('Help Out');
+    button.position(width/1.32,posY-width/42);
+    button.mousePressed();
+    button.size(width/9,height/18);
+    button.hide()
+    rect(posX+width/3.2,posY,width/8,height/16,10)
+    //help out text
+    fill(255)
+    textSize(24)
+    text("Help Out",posX+width/3.2,posY+height/85)
+  }
+  tasks(width/2,height/3.75,"Person Person","Email","1234567890","city","Deliver Groceries")
+  tasks(width/2,height/3.75+height/6,"Person Person","Email","1234567890","city","Deliver Groceries")
+  tasks(width/2,height/3.75+height/3,"Person Person","Email","1234567890","city","Deliver Groceries")
 }
--- covid-connections/Main/sketch.js
+++ covid-connections/Main/sketch.js
@@ -3,15 +3,6 @@ function setup() {
 
 }
 function draw() {
-  textAlign(CENTER)
-  rectMode(CENTER)
-  background(89, 85, 86)
-  noStroke();
-  fill(255)
-  textSize(48)
-  text("I Want To Help Others",width/2,height/15)
-  fill(203, 55, 58)
-  rect(width/2,height/2, width/1.1, height/1.25, 10)
 
 
 }
