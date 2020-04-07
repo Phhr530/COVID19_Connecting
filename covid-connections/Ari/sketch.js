3cfb9c93b4c0a136d583b4cedaaabaee6e900261
areskapoor <56699380+areskapoor@users.noreply.github.com>
2020-04-07 14:47:20 -0700

    safdsafs

--- covid-connections/Ari/sketch.js
+++ covid-connections/Ari/sketch.js
@@ -1,45 +1,45 @@
 let button
 function setup() {
-  createCanvas(windowWidth, windowHeight)
 
 }
 function draw() {
+  createCanvas(windowWidth, windowHeight)
   textAlign(CENTER)
   rectMode(CENTER)
   background(89, 85, 86)
   noStroke();
   //title text
   fill(255)
-  textSize(48)
-  text("I Want To Help Others",width/2,height/15)
+  textSize(width/18)
+  text("I Want To Help Others",windowWidth/2,windowHeight/15)
   //big box
   fill(203, 55, 58)
-  rect(width/2,height/2, width/1.1, height/1.25, 10)
+  rect(windowWidth/2,windowHeight/2, windowWidth/1.1, windowHeight/1.25, 10)
   //subtitle text
   fill(255)
-  textSize(36)
-  text("Tasks You Can Help With",width/2,height/6)
+  textSize(width/22)
+  text("Tasks You Can Help With",windowWidth/2,windowHeight/6)
   //tasks
   function tasks(posX,posY,fullName,email,phoneNum,city,task) {
     //main box text
-    rect(posX,posY,width/1.2,height/7,10)
+    rect(posX,posY,windowWidth/1.2,windowHeight/7,10)
     //name text
     fill(203, 55, 58)
-    textSize(20)
-    text(fullName,posX-width/3.25,posY-height/40)
+    textSize(width/40)
+    text(fullName,posX-windowWidth/3.25,posY-windowHeight/40)
     //help rectangle & button
     button = createButton('Help Out');
-    button.position(width/1.32,posY-width/42);
+    button.position(windowWidth/1.32,posY-windowWidth/42);
     button.mousePressed();
-    button.size(width/9,height/18);
+    button.size(windowWidth/9,windowHeight/18);
     button.hide()
-    rect(posX+width/3.2,posY,width/8,height/16,10)
+    rect(posX+windowWidth/3.2,posY,windowWidth/8,windowHeight/16,10)
     //help out text
     fill(255)
-    textSize(24)
-    text("Help Out",posX+width/3.2,posY+height/85)
+    textSize(width/38)
+    text("Help Out",posX+windowWidth/3.2,posY+windowHeight/85)
   }
-  tasks(width/2,height/3.75,"Person Person","Email","1234567890","city","Deliver Groceries")
-  tasks(width/2,height/3.75+height/6,"Person Person","Email","1234567890","city","Deliver Groceries")
-  tasks(width/2,height/3.75+height/3,"Person Person","Email","1234567890","city","Deliver Groceries")
+  tasks(windowWidth/2,windowHeight/3.75,"Person Person","Email","1234567890","city","Deliver Groceries")
+  tasks(windowWidth/2,windowHeight/3.75+windowHeight/6,"Person Person","Email","1234567890","city","Deliver Groceries")
+  tasks(windowWidth/2,windowHeight/3.75+windowHeight/3,"Person Person","Email","1234567890","city","Deliver Groceries")
 }
